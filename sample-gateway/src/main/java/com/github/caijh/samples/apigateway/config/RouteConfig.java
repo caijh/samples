28bf9ff8db38c267270d61bf58d7397a6f329795
蔡俊辉 <caiqizhe@gmail.com>
2019-08-13 11:00:52 +0800

    add spring cloud gateway sample

---
+++ sample-gateway/src/main/java/com/github/caijh/samples/apigateway/config/RouteConfig.java
@@ -0,0 +1,20 @@
+package com.github.caijh.samples.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                      //basic proxy
+                      .route(r -> r.path("/jd")
+                                   .uri("http://jd.com:80/").id("jd_route")
+                      ).build();
+    }
+
+}
