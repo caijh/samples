7cebbc1a3b4c7d8269407b8e982103f031669cd0
蔡俊辉 <caiqizhe@gmail.com>
2019-08-21 15:07:29 +0800

    feature: add sample-eureka-server

---
+++ sample-eureka-server/src/main/java/com/github/caijh/samples/sampleeurekaserver/SampleEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.github.caijh.samples.sampleeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SampleEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleEurekaServerApplication.class, args);
+    }
+
+}
