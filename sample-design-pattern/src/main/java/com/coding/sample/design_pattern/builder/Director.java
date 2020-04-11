5deadf690ce1709c65d102bd86a97ce4837ae18c
蔡俊辉 <caqizhe@gmail.com>
2020-04-11 19:40:23 +0800

    refactor: add test class

--- sample-design-pattern/src/main/java/com/coding/sample/design_pattern/builder/Director.java
+++ sample-design-pattern/src/main/java/com/coding/sample/design_pattern/builder/Director.java
@@ -1,5 +1,9 @@
 package com.coding.sample.design_pattern.builder;
 
+/**
+ * 导演类
+ * 负责安排已有的模块顺序，然后告诉Builder开始构建.
+ */
 public class Director {
 
     private Builder builder = new ConcreteProductBuilder();
