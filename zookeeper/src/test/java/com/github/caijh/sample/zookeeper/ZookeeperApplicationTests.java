a1e3d3332c681021868e50eb43bc77812e7ca897
蔡俊辉 <caqizhe@gmail.com>
2020-05-14 22:41:55 +0800

    feat: add zookeeper sample

---
+++ zookeeper/src/test/java/com/github/caijh/sample/zookeeper/ZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.caijh.sample.zookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZookeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
