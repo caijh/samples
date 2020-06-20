bd1670401f495dbf06d01e159bb558755719a90b
蔡俊辉 <caqizhe@gmail.com>
2020-06-20 09:59:08 +0800

    feat: init sample-websocket

---
+++ sample-websocket/src/main/java/com/github/caijh/sample/websocket/SampleWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.github.caijh.sample.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleWebsocketApplication.class, args);
+    }
+
+}
